dca8fbd1df383afeb5a5f4e47986fd212bbb59e1
Vanessa Maldonado <vbarajas4@gmail.com>
2021-08-03 03:14:34 -0700

    fixed the nav and added components to app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./App.css";
 import Nav from './components/Nav';
-import Greeting from './components/Greeting';
 import Footer from './components/Footer';
 import About from "./components/pages/About";
-// import Contact from "./components/pages/Contact";
+import Contact from "./components/pages/Contact";
 import Portfolio from "./components/pages/Portfolio";
-// import Resume from "./components/pages/Resume";
+import Resume from "./components/pages/Resume";
+//import Wrapper from './components/Wrapper';
 
 function App() {
+
+  // const [currentPage, setCurrentPage] = useState('About');
+  
+  // // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  // const renderPage = () => {
+  //   
+  //   if (currentPage === 'About') {
+  //     return <About />;
+  //   }
+  //   if (currentPage === 'Portfolio') {
+  //     return <Portfolio />;
+  //   }
+  //   if (currentPage === 'Resume') {
+  //     return <Resume />;
+  //   }
+  //   return <Contact />;
+  // };
+
+  // const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <div>
       <Nav />
-      <Greeting />
       <About />
       <Portfolio />
+      <Contact />
+      <Resume />
       <Footer />
+      
 
     
     </div>
@@ -26,28 +48,15 @@ export default App;
 
 
 
-// import logo from './logo.svg';
-// import './App.css';
 
-// function App() {
+
+
 //   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
+//     <div>
+//       {/* We are passing the currentPage from state and the function to update it */}
+//       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+//       {/* Here we are calling the renderPage method which will return a component  */}
+//       {renderPage()}
 //     </div>
 //   );
 // }
-
-// export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,34 +1,64 @@
 import React from 'react';
 
 export default function Nav() {
-    const linkStyle = { border: '1px black', padding: '15px' };
+    
+  
   
     return (
-      <nav className="main-header-menu">
-        <section
-          style={{
-            display: 'flex',
-            fontFamily: 'helvetica',
-            flexDirection: 'row',
-            alignItems: 'flex-start',
-            justifyContent: 'flex-end',
-          }}
-        >
- 
-          <div style={linkStyle}>
-            <a href="#">About</a>
-          </div>
-          <div style={linkStyle}>
-            <a href="#">Portfolio</a>
-          </div>
-          <div style={linkStyle}>
-            <a href="#">Resume</a>
-          </div>
-          <div style={linkStyle}>
-            <a href="#">Contact</a>
-          </div>
-        </section>
+
+      <nav className="navbar navbar-light bg-primary">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1 fs-2 fw-bold p-2">Vanessa Maldonado</span>
+  
+            <ul className="nav justify-content-end" >
+              <li className="nav-item" >
+                <a className="nav-link active text-dark h5" aria-current="page" href="/">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-dark h5" href="/Portfolio">Portfolio</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-dark h5" href="/Contact">Contact</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-dark h5" href="/Resume">Resume</a>
+              </li>
+
+            </ul>
+        </div>
       </nav>
     );
   }
   
+
+
+
+// const linkStyle = { border: '1px black', padding: '15px' };
+
+
+
+    //     <nav className="main-header-menu">
+  //       <section
+  //         style={{
+  //           display: 'flex',
+  //           fontFamily: 'helvetica',
+  //           flexDirection: 'row',
+  //           alignItems: 'flex-start',
+  //           justifyContent: 'flex-end',
+  //         }}
+  //       >
+ 
+  //         <div style={linkStyle}>
+  //           <a href="#">About</a>
+  //         </div>
+  //         <div style={linkStyle}>
+  //           <a href="#">Portfolio</a>
+  //         </div>
+  //         <div style={linkStyle}>
+  //           <a href="#">Resume</a>
+  //         </div>
+  //         <div style={linkStyle}>
+  //           <a href="#">Contact</a>
+  //         </div>
+  //       </section>
+  //     </nav>
