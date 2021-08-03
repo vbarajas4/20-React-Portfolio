a30fa992ba63784bfa2b1dc05f381dc09843b9a4
Vanessa Maldonado <vbarajas4@gmail.com>
2021-08-03 03:11:05 -0700

    added resume pdf and bullet points

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,1 +1,36 @@
 import React from 'react';
+
+export default function Resume() {
+
+    return (
+        <div>
+            <h1>Resume</h1>
+
+            <div>
+                <p>Download my <a href="../../assets/VM-Resume-ATS.pdf">Resume</a></p>
+
+                <h2>Front-End Proficiencies</h2>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>JQuery</li>
+                    <li>React</li>
+                    <li>Bootstrap</li>           
+                </ul>
+
+                <h2>Back-End Proficiencies</h2>
+                <ul>
+                    <li>APIs</li>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>MySQL</li>                    
+                    <li>MVC</li>
+                    <li>MongoDB</li>
+            
+                </ul>
+            </div>
+        </div>
+
+    )
+}
