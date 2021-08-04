28e03ff3baa58e50033233c8a9cf764c8291423f
Vanessa Maldonado <vbarajas4@gmail.com>
2021-08-04 02:06:44 -0700

    added projects to component

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,67 +1,86 @@
 import React from 'react';
+import { Router } from 'react-router';
 
+
+const containerStyle = {
+    display:'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    position: 'relative',
+    height: '70%',
+    width: '70%',
+    marginTop: '70px',
+    marginLeft: '20px',
+    marginBottom: '70px',
+   
+}
 function Portfolio() {
     return (
 
-        <section className="container" id="projects">
-            <h2 className="container-title">Projects</h2>
-            <div className="grid-layout">
-
-                <div className="web-image project-1" id="full-size-image">  
-                    <a href="https://damaximum.github.io/Productivity-Hub/">
-                        <img src="./assets/images/Productivity-Hub-SS.png" alt="Productivity Hub Screenshot" className="screenshot"/></a>
-                    <div className="image-title">
-                        <h3>Productivity Hub</h3>
-                        <p>HTML/CSS/JS/API</p>        
-                        <a href="https://github.com/Damaximum/Productivity-Hub">GitHub Link</a>
-                    </div>
-                </div>
-
-                <div className="project-2 web-image">
-                    <a href="https://vbarajas4.github.io/03-HW-Password-Generator/">
-                        <img src="./assets/images/Password-Generator-SS.png" alt="Password Generator screenshot" className="screenshot"/></a>
-                    <div className="image-title">
-                        <h3>Password Generator</h3>
-                        <p>JavaScript</p> 
-                        <a href="https://github.com/vbarajas4/03-HW-Password-Generator">GitHub Link</a>
-                    </div>
-                </div>
-
-                <div className="project-3 web-image">
-                    <a href="https://vbarajas4.github.io/05-Third-Party-API-WorkDayScheduler/">
-                        <img src="./assets/images/Daily-Planner-Blank.png" alt="work schedule screenshot" className="screenshot"/></a>
-                    <div className="image-title">
-                        <h3>Daily Work Planner</h3>
-                        <p>Third Party API</p>
-                        <a href="https://github.com/vbarajas4/05-Third-Party-API-WorkDayScheduler">GitHub Link</a>
-                    </div>
-                </div>
-
-                <div className="project-4 web-image">
-                    <a href="https://vbarajas4.github.io/06-Server-Side-APIs-Weather-Dashboard/">
-                        <img src="./assets/images/Weather-Dashboard.png" alt="Weather dashboard" className="screenshot"/></a>
-                    <div className="image-title">
-                        <h3>Weather Dashboard</h3>
-                        <p>Server Side API</p>
-                        <a href="https://github.com/vbarajas4/06-Server-Side-APIs-Weather-Dashboard">GitHub Link</a>
-                    </div>
-                </div>
-
-                <div className="project-5 web-image">
-                    <a href="https://drive.google.com/file/d/1s9tP2GXIVzUjj3-pFf9cVIeCK2VXAXbX/view">
-                    <img src="./assets/images/Team-Profile-Cards.png" alt="Profile Cards" className="screenshot"/> </a>
-                    <div className="image-title">
-                        <h3>Team Profile</h3>
-                        <p>OOP</p>
-                        <a href="https://github.com/vbarajas4/10-OOP-Team-Profile">GitHub Link</a>
-                    </div>
-               
-                </div>
+  <div className="container" style = { containerStyle }>
+    <div className="row row-cols-1 row-cols-md-3 g-4">
+        <div className="col">
+            <div className="card h-100">
+              <a href="https://damaximum.github.io/Productivity-Hub/">
+                <img src={require("../../assets/Productivity-Hub-SS.png").default} className="card-img-top" alt="Productivity Hub"/></a>
+              <div className="card-body">
+                <h5 className="card-title">Productivity Hub</h5>
+                <p className="card-text"> A productivity app that helps you stay focus and stay on task.</p>
+                <a href="https://github.com/Damaximum/Productivity-Hub" className="btn btn-primary">View Github Project</a>
+              </div>
             </div>
-                      
-        </section>
-    )
+        </div>
 
+        <div className="col">
+            <div className="card h-100">
+              <a href="https://drive.google.com/file/d/1s9tP2GXIVzUjj3-pFf9cVIeCK2VXAXbX/view">
+                <img src={require("../../assets/Team-Profile-Cards.png").default} className="card-img-top" alt="Profile cards"/></a>
+              <div className="card-body">
+                <h5 className="card-title">Team Profile</h5>
+                <p className="card-text"> An application that helps you keep track of your employees profile.</p>
+                <a href="https://github.com/vbarajas4/10-OOP-Team-Profile" className="btn btn-primary">View Github Project</a>
+              </div>
+            </div>
+        </div>
+
+        <div className="col">
+            <div className="card h-100">
+              <a href="https://vbarajas4.github.io/05-Third-Party-API-WorkDayScheduler/">
+                <img src={require("../../assets/Daily-Planner-Blank.png").default} className="card-img-top" alt="work schedule"/></a>
+              <div className="card-body">
+                <h5 className="card-title">Daily Work Planner</h5>
+                <p className="card-text">An application that allows the user to see their appointments for the day color coding times by past, present, and future events .</p>
+                <a href="https://github.com/vbarajas4/05-Third-Party-API-WorkDayScheduler" className="btn btn-primary">View Github Project</a>      
+              </div>
+            </div>
+        </div>
+
+        <div className="col">
+            <div className="card h-100">
+              <a href="https://vbarajas4.github.io/06-Server-Side-APIs-Weather-Dashboard/">
+                <img src={require("../../assets/Weather-Dashboard.png").default} className="card-img-top" alt="Productivity Hub"/></a>
+              <div className="card-body">
+                <h5 className="card-title">Weather Dashboard</h5>
+                <p className="card-text"> An application that lets the user see the current weather and the forecast for the next 5 days.</p>
+                <a href="https://github.com/vbarajas4/06-Server-Side-APIs-Weather-Dashboard" className="btn btn-primary">View Github Project</a>
+              </div>
+            </div>
+        </div>
+
+        <div className="col">
+            <div className="card h-100">
+              <a href="https://vbarajas4.github.io/03-HW-Password-Generator/">
+                <img src={require("../../assets/Password-Generator-SS.png").default} className="card-img-top" alt="Password Generator"/></a>
+              <div className="card-body">
+                <h5 className="card-title">Password Generator</h5>
+                <p className="card-text">An application that generates a random password for a user based on the criteria selected</p>
+                <a href="https://github.com/vbarajas4/03-HW-Password-Generator" className="btn btn-primary">View Github Project</a>   
+              </div>
+            </div>
+        </div>   
+    </div>
+  </div>
+    )
 }
 
 export default Portfolio;
